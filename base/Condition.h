4d795b32a86289e6732aa443c0e2cfb3b7ec7fc2
Goastcjh <545660123@qq.com>
2021-12-13 16:38:02 +0800

    commit

--- base/Condition.h
+++ base/Condition.h
@@ -10,19 +10,19 @@ class Condition : noncopyable {
 public:
     //显示构造
     explicit Condition(MutexLock &mutex) : mutex_(mutex) {
-        pthread_cond_init(&pcond_, NULL);
+        pthread_cond_init(&pcond_, NULL); //初始化条件变量
     }
 
     ~Condition() {
-        pthread_cond_destroy(&pcond_);
+        pthread_cond_destroy(&pcond_); //释放条件变量
     }
 
     void wait() {
-        pthread_cond_signal(&pcond_);
+        pthread_cond_wait(&pcond_, mutex_.getPthreadMutex()); //阻塞在条件变量上
     }
 
     void notify() {
-        pthread_cond_signal(&pcond_);
+        pthread_cond_signal(&pcond_);  //解除在条件变量上的阻塞
     }
 
     void notifyAll() {
@@ -39,7 +39,7 @@ public:
         abstime.tv_sec += static_cast<time_t>((abstime.tv_nsec + nanoseconds) / kNanoSecondsPerSecond);
         abstime.tv_nsec = static_cast<long>((abstime.tv_nsec + nanoseconds) % kNanoSecondsPerSecond);
 
-        return ETIMEDOUT == pthread_cond_timedwait(&pcond_, mutex_.getPthreadMutex(), &abstime);
+        return ETIMEDOUT == pthread_cond_timedwait(&pcond_, mutex_.getPthreadMutex(), &abstime);  //阻塞直到指定时间
     }
 private:
     MutexLock& mutex_;
